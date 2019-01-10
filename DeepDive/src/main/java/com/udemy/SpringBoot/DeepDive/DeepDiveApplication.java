aec877e00893cefc56d3d4748f7f8e136ad4b050
Alasgar Baghirli <bagirlielesger@gmail.com>
2019-01-10 16:11:49 +0400

    create deep dive app

---
+++ DeepDive/src/main/java/com/udemy/SpringBoot/DeepDive/DeepDiveApplication.java
@@ -0,0 +1,14 @@
+package com.udemy.SpringBoot.DeepDive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepDiveApplication.class, args);
+	}
+
+}
+
