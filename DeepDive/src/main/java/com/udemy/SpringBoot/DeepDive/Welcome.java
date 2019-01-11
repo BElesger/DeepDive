2ecf5c83c086898562ed7769394006c860e8cb99
BAlasgar <bagirlielesger@gmail.com>
2019-01-11 10:51:35 +0400

    add welcome restService

--- DeepDive/src/main/java/com/udemy/SpringBoot/DeepDive/Welcome.java
+++ DeepDive/src/main/java/com/udemy/SpringBoot/DeepDive/Welcome.java
@@ -1,4 +1,30 @@
 package com.udemy.SpringBoot.DeepDive;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Welcome {
+
+    @Autowired
+    private WelcomeService welcomeService;
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+        return welcomeService.getWelcomeMessage();
+    }
+
+
+}
+
+@Service
+class WelcomeService {
+
+    public String getWelcomeMessage() {
+        return "Welcome Message !!!";
+    }
 }
